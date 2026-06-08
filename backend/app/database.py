7afe4cec631ed6af8ba12759294f2a4caf94675e
aiden <an010719@naver.com>
2026-06-08 09:48:31 +0900

    백엔드 배포 관련 수정

--- backend/app/database.py
+++ backend/app/database.py
@@ -5,7 +5,9 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_URL = os.getenv("DATABASE_URL", "").replace(
+    "mysql://", "mysql+pymysql://"
+)
 
 if DATABASE_URL is None:
     raise ValueError("DATABASE_URL 환경 변수가 설정되지 않았습니다. .env 파일을 확인하세요.")
